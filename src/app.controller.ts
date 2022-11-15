31325ede8b354ff906fdc6908eb5923b422b47a5
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2022-11-15 09:06:34 -0500

    9.Separacion de responsabilidades

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -13,32 +13,5 @@ export class AppController {
   @Get('nuevo')
   newEndPoint() {
     return 'Nuevo endpoint';
-  }
-
-  @Get('products')
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    return `products  limit => ${limit} offset ${offset} brand => ${brand}`;
-  }
-
-  @Get('products/filter')
-  getProductFilter() {
-    return 'yo soy un filtro';
-  }
-
-  @Get('products/:productiId')
-  getProduct(@Param('productiId') productId: string) {
-    return `product ${productId}`;
-  }
-
-  @Get('categories/:categoryId/products/:productId')
-  getCategory(
-    @Param('categoryId') categoryId: string,
-    @Param('productId') productId: string,
-  ) {
-    return `categoria ${categoryId} y producto ${productId}`;
   }
 }
