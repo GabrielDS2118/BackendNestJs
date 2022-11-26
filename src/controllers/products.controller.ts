405bece75d26f6bfbdc3a170782ee80ca235f67b
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2022-11-26 12:22:25 -0500

    15.Implementando servicios en controladores

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -11,15 +11,20 @@ import {
   HttpCode,
 } from '@nestjs/common';
 
+import { ProductsService } from '../services/products.service';
+
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
   @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `products  limit => ${limit} offset ${offset} brand => ${brand}`;
+    // return `products  limit => ${limit} offset ${offset} brand => ${brand}`;
+    return this.productsService.findAll();
   }
 
   @Get('filter')
@@ -30,23 +35,28 @@ export class ProductsController {
   @Get(':productiId')
   @HttpCode(HttpStatus.ACCEPTED)
   getProduct(@Param('productiId') productId: string) {
-    return `product ${productId}`;
+    // return `product ${productId}`;
+    return this.productsService.findOne(+productId);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'acccion para crear',
-      payload,
-    };
+    // return {
+    //   message: 'acccion para crear',
+    //   payload,
+    // };
+
+    return this.productsService.create(payload);
   }
 
   @Put(':productId')
   update(@Param('productId') productId: number, @Body() payload: any) {
-    return {
-      productId,
-      payload,
-    };
+    // return {
+    //   productId,
+    //   payload,
+    // };
+
+    return this.productsService.update(productId, payload);
   }
 
   @Delete(':productId')
